b27d3bb9935feae7165287c5c54c0388c579235d
talhanust <38860520+talhanust@users.noreply.github.com>
2025-09-19 11:45:25 +0500

    Update vite.config.ts

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/engineering-app/' : '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
   server: {
     port: 5173,
     host: true,
